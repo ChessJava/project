85f583c9529a76074241c0b9965f2d49e383b923
ChessJava <249534284@qq.com>
2019-07-23 20:29:33 +0800

    Third Commit Time

--- src/test/java/TestCode4.java
+++ src/test/java/TestCode4.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import TimeChange.TimeChange;
 
 public class TestCode4 {
+    public static void main(String[] args) {
+
+        TimeChange tc = new TimeChange();
+        tc.show();
+    }
 }
