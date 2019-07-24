b0ce8d787e70c771f9c0ec6ca0490569e6319663
ChessJava <249534284@qq.com>
2019-07-24 21:20:16 +0800

    Log Add To System For Test

--- src/test/java/TestCode3.java
+++
@@ -1,5 +0,0 @@
-public class TestCode3 {
-    public static void main(String[] args) {
-        Thread.yield();
-    }
-}
--- src/test/java/TestCode4.java
+++
@@ -1,9 +0,0 @@
-import TimeChange.TimeChange;
-
-public class TestCode4 {
-    public static void main(String[] args) {
-
-        TimeChange tc = new TimeChange();
-        tc.show();
-    }
-}
