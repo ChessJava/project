db7c823b513510ab1fbc82286ec2e4cec2c8d587
ChessJava <249534284@qq.com>
2019-07-25 16:46:53 +0800

    Update notes

--- src/main/java/com/test/interceptor/WebappAdapter.java
+++ src/main/java/com/test/interceptor/WebappAdapter.java
@@ -11,8 +11,12 @@ public class WebappAdapter implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		MyInterceptor mi = new MyInterceptor();
+		// 将自定义拦截器加入到MVC拦截器管理中心
 		registry.addInterceptor(mi)
-				.addPathPatterns("/**").excludePathPatterns(mi.getUrl());
+				// 设置拦截的请求，这里的"**"为拦截所有请求
+				.addPathPatterns("/**")
+				// 设置不拦截的请求
+				.excludePathPatterns(mi.getUrl());
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 	
